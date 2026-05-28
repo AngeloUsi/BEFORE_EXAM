c56b2eff8f6c242e7275dcd4f59fbf7cf985684a
unknown <Angelolboy@gmail.com>
2026-05-28 11:37:17 +0800

    Fixed Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\employeecontroller;
+
 
 
 
@@ -17,24 +19,16 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
-    
+
     Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
     Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
-    Route::('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
     Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index']);
-    Route::('employee/create', [App\Http\Controllers\employeecontroller::class, 'create']);
-    Route::('employee', [App\Http\Controllers\employeecontroller::class, 'store']);
-    // Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit']);
-    // Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update']);
-    // Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete']);
-
-    
-
-
-
-
-
-
+    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create']);
+    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store']);
+    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit']);
+    Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update']);
+    Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete']);
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 });
